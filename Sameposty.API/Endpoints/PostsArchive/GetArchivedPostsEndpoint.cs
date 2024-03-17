e0a3fe150c02082bb0e34164fae47bb4b5280c09
Marek Jakubicki <jakubicki.m@gmail.com>
2024-03-17 01:09:45 +0100

    Fix publishing

---
+++ Sameposty.API/Endpoints/PostsArchive/GetArchivedPostsEndpoint.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+using Sameposty.DataAccess.Executors;
+using Sameposty.DataAccess.Queries.Posts;
+
+namespace Sameposty.API.Endpoints.PostsArchive;
+
+public class GetArchivedPostsEndpoint(IQueryExecutor queryExecutor) : EndpointWithoutRequest
+{
+    public override void Configure()
+    {
+        Get("archive");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var id = User.FindFirst("UserId").Value;
+        var loggedUserId = int.Parse(id);
+
+        var archivedPostsquery = new GetArchivedPostsByUserId() { UserId = loggedUserId };
+        var archivedPosts = await queryExecutor.ExecuteQuery(archivedPostsquery);
+
+        await SendOkAsync(archivedPosts, ct);
+    }
+}
