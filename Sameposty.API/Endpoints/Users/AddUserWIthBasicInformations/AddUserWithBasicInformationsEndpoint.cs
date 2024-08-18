5d1a06cfce33e7c41c2774f3e446e85d4af4ed42
Marek Jakubicki <jakubicki.m@gmail.com>
2024-08-19 00:28:22 +0200

    Add new user endpoint and update user model attributes (#8)
    - Modified `User` class in `User.cs` to remove `[Required]` and `[MaxLength(10)]` attributes from `NIP` property and set its default value to an empty string.
    - Updated `SamepostyDbContextModelSnapshot.cs` to include `(string)null` in `b.ToTable` method calls for multiple tables.
    - Simplified the message in `RegisterConfirmationEmailBody` method in `EmailBodyProvider.cs`.
    - Added `AddUserWithBasicInformationsEndpoint` class in `AddUserWithBasicInformationsEndpoint.cs` to handle new user creation with additional basic information.
    - Added `AddUserWithBasicInformationsRequest` class in `AddUserWithBasicInformationsRequest.cs` to extend `AddUserRequest` with additional properties.
    
    Co-authored-by: MarekJakubicki <marek.jakubicki@plte.pl>

---
+++ Sameposty.API/Endpoints/Users/AddUserWIthBasicInformations/AddUserWithBasicInformationsEndpoint.cs
@@ -0,0 +1,66 @@
+﻿using FastEndpoints;
+using Sameposty.DataAccess.Commands.Users;
+using Sameposty.DataAccess.Entities;
+using Sameposty.DataAccess.Executors;
+using Sameposty.DataAccess.Queries.Users;
+using Sameposty.Services.Configurator;
+using Sameposty.Services.EmailService;
+using Sameposty.Services.Hasher;
+using Sameposty.Services.JWTService;
+
+namespace Sameposty.API.Endpoints.Users.AddUserWIthBasicInformations;
+
+public class AddUserWithBasicInformationsEndpoint(IQueryExecutor queryExecutor, IConfigurator configurator, ICommandExecutor commandExecutor, IJWTBearerProvider jwt, IEmailService email) : Endpoint<AddUserWithBasicInformationsRequest>
+{
+    public override void Configure()
+    {
+        Verbs(Http.POST);
+        Routes("addUserWithBasicInformations");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(AddUserWithBasicInformationsRequest req, CancellationToken ct)
+    {
+        var getUserByEmail = new GetUserByEmailQuery(req.Email);
+        var userFromDb = await queryExecutor.ExecuteQuery(getUserByEmail);
+
+        if (userFromDb != null)
+        {
+            ThrowError("Nie można użyć tego adresu e-mail");
+        }
+
+        var salt = Hasher.GetSalt();
+        var passwordHashed = Hasher.HashPassword(req.Password, salt);
+
+        var user = new User()
+        {
+            CreatedDate = DateTime.UtcNow,
+            Email = req.Email,
+            Password = passwordHashed,
+            Salt = salt,
+            NIP = req.NIP,
+            ImageTokensLimit = configurator.ImageTokensDefaultLimit,
+            TextTokensLimit = configurator.TextTokensDefaultLimit,
+            Role = DataAccess.Entities.Roles.FreeUser,
+            BasicInformation = new BasicInformation()
+            {
+                BrandName = req.BrandName,
+                Audience = req.Audience,
+                Mission = req.Mission,
+                ProductsAndServices = req.ProductsAndServices,
+                Goals = req.Goals,
+                Assets = req.Assets
+            }
+        };
+
+        var command = new AddUserCommand() { Parameter = user };
+
+        var newUserFromDb = await commandExecutor.ExecuteCommand(command);
+
+        var newToken = jwt.ProvideToken(newUserFromDb.Id.ToString(), newUserFromDb.Email, newUserFromDb.Role.ToString());
+
+        await email.SendRegisterConfirmationEmail(req.Email, newToken);
+
+        await SendOkAsync($"{user.Email}", ct);
+    }
+}
