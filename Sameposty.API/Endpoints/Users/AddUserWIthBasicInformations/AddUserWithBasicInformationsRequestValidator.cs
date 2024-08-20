e750e7bfdb5b55dcda8ed7832f56637d708bace2
MarekJakubicki <marek.jakubicki@plte.pl>
2024-08-20 16:45:56 +0200

    Add user request validator with FluentValidation rules
    Introduce AddUserWithBasicInformationsRequestValidator class in
    Sameposty.API.Endpoints.Users.AddUserWIthBasicInformations namespace.
    This class inherits from Validator<AddUserWithBasicInformationsRequest>
    and defines validation rules using FluentValidation.
    
    Validation rules:
    - Email: required, max 50 chars, valid format
    - Password: required, 4-20 chars
    
    Error messages are provided in Polish.

---
+++ Sameposty.API/Endpoints/Users/AddUserWIthBasicInformations/AddUserWithBasicInformationsRequestValidator.cs
@@ -0,0 +1,30 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Sameposty.API.Endpoints.Users.AddUserWIthBasicInformations;
+
+public class AddUserWithBasicInformationsRequestValidator : Validator<AddUserWithBasicInformationsRequest>
+{
+    public AddUserWithBasicInformationsRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotNull()
+            .WithMessage("Email nie może być pusty")
+            .NotEmpty()
+            .WithMessage("Email nie może być pusty")
+            .MaximumLength(50)
+            .WithMessage("Email nie więcej niż 50 znaków")
+            .EmailAddress()
+            .WithMessage("E-mail ma nieprawidłowy format");
+
+        RuleFor(x => x.Password)
+            .NotNull()
+            .WithMessage("Hasło nie może być puste")
+            .NotEmpty()
+            .WithMessage("Hasło nie może być puste")
+            .MaximumLength(20)
+            .WithMessage("Hasło nie więcej niż 20 znaków")
+            .MinimumLength(4)
+            .WithMessage("Hasło nie mniej niż 4 znaki");
+    }
+}
